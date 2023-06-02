635d3a0218f3c1009ddcf32d74754d6862c1ef9f
Messerschmidts101 <promanners@gmail.com>
2023-06-02 16:30:43 +0800

    2nd update

--- model/churn_prediction.py
+++ model/churn_prediction.py
@@ -1,3 +1,40 @@
+"""
+[4250 rows x 19 columns]
+Confusion matrix: 
+ [[738   4]
+ [ 31  77]]
+Precision:  0.9506172839506173
+Recall:  0.7129629629629629
+F-BETA:  0.7504873294346978
+Feature importance:
+state :  0.02091085773356425
+account_length :  0.02364933536951014
+area_code :  0.005446811097901077
+international_plan :  0.09008765128451562
+voice_mail_plan :  0.018082811468269604
+number_vmail_messages :  0.02562426090857211
+total_day_minutes :  0.15240994265109628
+total_day_calls :  0.024252578124440406
+total_day_charge :  0.1463627193163896
+total_eve_minutes :  0.06244392907894073
+total_eve_calls :  0.020166899254556468
+total_eve_charge :  0.06205752452374809
+total_night_minutes :  0.03484614057463236
+total_night_calls :  0.02328064725325476
+total_night_charge :  0.03403347558864758
+total_intl_minutes :  0.03987570130753387
+total_intl_calls :  0.04433202680088285
+total_intl_charge :  0.036752017833653165
+number_customer_service_calls :  0.13538466982989109
+
+[[THINGS NEEDED TO WORK ON]]
+1. Imputing missing values
+2. Encoded variables [done]
+3. Handling imbalanced dataset
+4. Feature engineering
+
+"""
+
 import pandas as pd
 import numpy as np
 import joblib
@@ -26,6 +63,7 @@ from nltk.corpus import stopwords
 
 
 def divide_dataset(data_set):
+    pass
 
 def one_hot_encode(data_set):
     # Perform one-hot encoding
@@ -54,6 +92,8 @@ def fitChurn():
     '''=========================I.C. DATA PREPARATION: DATA ENCODING========================='''
     x = one_hot_encode(x)
     print("after encoding: \n",x)
+    # Convert x to CSV
+    x.to_csv('model/data/x_data.csv', index=False)
     '''=========================I.D. DATA PREPARATION: FEATURE ENGINERING========================='''
     '''=========================I.E. DATA PREPARATION: TRAIN/TEST SPLIT========================='''
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.20, random_state=123)
@@ -89,6 +129,7 @@ def fitChurn():
     '''=========================V.I EXPORTING: FINAL TRAINING========================='''
     classifier = RandomForestClassifier(n_estimators=grid_search.best_params_['n_estimators'], max_depth=grid_search.best_params_['max_depth'], criterion="entropy")
     classifier.fit(x_train, y_train)
+    print("classifier: ",classifier)
     '''=========================V.I EXPORTING: FINAL TESTING AND EVALUATION========================='''
     # Predicting the test set result
     y_pred = classifier.predict(x_test)
@@ -112,60 +153,3 @@ def fitChurn():
     # Print the classes
     print("Classes: ",classes)
 fitChurn()
-"""
-    # Check which columns have NaN values after filling
-    #print("Columns with NaN values after filling:\n", x.isna().sum())
-
-    # Splitting the dataset into training and test set
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.20, random_state=40)
-
-    '''=========================TRAINING DATA========================='''
-    # Fitting Random Forest classifier to the training set
-    classifier = RandomForestClassifier(n_estimators=400, max_depth=10, criterion="entropy")
-    classifier.fit(x_train, y_train)
-
-    '''=========================TESTING DATA========================='''
-    # Predicting the test set result
-    y_pred = classifier.predict(x_test)
-    # Creating the confusion matrix and computing precision, recall, and F-beta score
-    cm = confusion_matrix(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, pos_label='bot')
-    recall = recall_score(y_test, y_pred, pos_label='bot')
-    f1beta = fbeta_score(y_test, y_pred, beta=2, pos_label='bot')
-    # Printing the results and feature importance
-    print("Confusion matrix: \n", cm)
-    print("Precision: ", precision, "\nRecall: ", recall, "\nF-BETA: ", f1beta)
-    print("Feature importance: ")
-    for index, feature in enumerate(['followers_count',
-              	'friends_count',
-               	'listed_count',	
-                'favourites_count',	
-                'statuses_count',	
-                'verified',	
-                'description_text_mining_result',	
-                'description_link_count',	
-                'description_hashtags_count',	
-                'description_mentions_count',	
-                'tweet_rate']):
-        print(feature,": ",classifier.feature_importances_[index])
-    
-    '''=========================HYPERPARAMETER TUNING========================='''
-    param_grid = {
-        'n_estimators': [100, 200, 300, 400],
-        'max_depth': [5, 10, 15, 20]
-    }
-    # Perform grid search with 5-fold cross-validation
-    grid_search = GridSearchCV(estimator = classifier, param_grid = param_grid, cv=5, n_jobs = -1, verbose = 2)
-    grid_search.fit(x_train, y_train)
-    # Print the best hyperparameters and corresponding score
-    print("Best hyperparameters:", grid_search.best_params_)
-    print("Best score:", grid_search.best_score_)
-
-    '''=========================EXPORTING MODEL========================='''
-    # Exporting the trained tree
-    joblib.dump(classifier, 'model/classifier_accInfo.pkl')
-    RandomForestTextMining = joblib.load('model/classifier_accInfo.pkl')
-    # Get the list of classes
-    classes = RandomForestTextMining.classes_
-    # Print the classes
-    print("Classes: ",classes)"""
--- server.py
+++ server.py
@@ -1,20 +1,28 @@
 from flask import Flask,request,jsonify,render_template,session,send_from_directory
+import joblib
+import pandas as pd
+import numpy as np
+
+# Load classifier
+churnprediction = joblib.load('model/churnprediction.pkl')
 
 # Create flask app
 flask_app = Flask(__name__,static_folder='static')
-
 
 @flask_app.route("/")
 def Home():
     return render_template("index.html")
 
-@flask_app.route("/_get_data", methods = ["POST",'GET'])
+@flask_app.route("/predict", methods = ["POST","GET"])
 def predict():
-    data = request.get_json()
-    print("DATA FROM JS: ",data)
-    return data
-    #return '', 200
-
+    if request.method == "POST":
+        float_features = [float(x) for x in request.form.values()]
+        features = [np.array(float_features)]
+        prediction = churnprediction.predict_proba(features)[0][0]
+        print("Prediction: ", prediction)
+        return render_template("index.html", Prediction_Here="The customer churn probability is: {}".format(prediction))
+    else:
+        return "Method Not Allowed"
 
 if __name__ == "__main__":
     flask_app.run(debug=True, host='0.0.0.0',port=8080)
