cde0de547acda45be51be18a8c4bd76ac1615178
Der Kreidekönig <mcolaguer@iskolarngbayan.pup.edu.ph>
2023-07-04 17:33:17 +0800

    deleted server files

--- server/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request
-from model import Customer
-
-api = Blueprint('api', __name__)
-
-@api.route("/predict", methods = ["POST", "GET"])
-def predict():
-    customer = request.get_json()
-    customer_predict = Customer()
